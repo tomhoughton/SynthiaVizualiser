6678e3db5f068ccb093531720150b6cc99aa1bf8
Thomas Houghton <tomhoughton888@gmail.com>
2022-08-04 22:43:24 +0100

    Inital

---
+++ chomra_disc.py
@@ -0,0 +1,27 @@
+# Imports:
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+def seperate_h_s(data, sr):
+    harm, percus = librosa.effects.hpss(data)
+
+    return harm, percus
+
+def main():
+    data, sr = librosa.load('flume-hollow.mp3')
+
+    data_h, data_p = seperate_h_s(data, sr)
+
+    chroma = librosa.feature.chroma_cqt(y=data_h, sr=sr)
+    plt.figure(figsize=(18,5))
+
+    librosa.display.specshow(chroma, sr=sr, x_axis='time', y_axis='chroma', vmin=0, vmax=1)
+    plt.title('Chromagram')
+    plt.colorbar()
+    plt.figure(figsize=(20,8))
+
+
+main()
---
+++ notes.py
@@ -0,0 +1,79 @@
+#%%
+
+import librosa
+
+#%%
+data, sr = librosa.load('flume-hollow.mp3')
+
+print('Data Shape: ', data.shape)
+
+# %%
+
+print('Sample Rate: ', sr)
+
+# %%
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+plt.plot(data)
+plt.show()
+
+# %%
+
+print(data)
+
+# %%
+import librosa.display
+spec = librosa.feature.melspectrogram(y=data, sr=sr)
+librosa.display.specshow(spec, y_axis='mel', x_axis='s', sr=sr)
+plt.colorbar() # This doesn't give much information.
+# Mel is used to display pitch in a more regularized distribution.
+
+# %%
+import numpy as np
+# We need to convert the amplitude squared to decibels.
+db_spec = librosa.power_to_db(spec, ref=np.max)
+librosa.display.specshow(db_spec, y_axis='mel', x_axis='s', sr=sr)
+plt.colorbar()
+# %%
+
+from IPython.display import Audio
+Audio(data=data, rate=sr)
+
+#%%
+
+# Split audio into harmonics and percussive:
+data_harm, data_perc = librosa.effects.hpss(data)
+
+spec_h = librosa.feature.melspectrogram(data_harm, sr=sr)
+spec_p = librosa.feature.melspectrogram(data_perc, sr=sr)
+
+db_spec_h = librosa.power_to_db(spec_h,ref=np.max)
+db_spec_p = librosa.power_to_db(spec_p,ref=np.max)
+
+#%%
+librosa.display.specshow(db_spec_h, y_axis='hz', x_axis='s', sr=sr)
+
+# %%
+librosa.display.specshow(db_spec_p, y_axis='hz', x_axis='s', sr=sr)
+
+# %%
+
+chroma = librosa.feature.chroma_cqt(y=data_harm, sr=sr)
+plt.figure(figsize=(18,5))
+
+librosa.display.specshow(chroma, sr=sr, x_axis='time', y_axis='chroma', vmin=0, vmax=1)
+plt.title('Chromagram')
+plt.colorbar()
+plt.figure(figsize=(20,8))
+
+# %%
+
+chroma.shape
+
+# %%
+print(chroma)
+
+# %%
+# https://medium.com/@patrickbfuller/librosa-a-python-audio-libary-60014eeaccfb 
